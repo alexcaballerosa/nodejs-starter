47a09fbf7cc2ae6f7065d4f00c919f3974c5bd0c
Alex Caballero <alex.caballero.4@gmail.com>
2021-10-21 21:10:24 +0200

    Add Express to project
    Install & setup Express.
    
    Install CORS & add as middleware.
    
    Add example HTML response in root endpoint.
    
    Create example GET endpoint.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,31 @@
-// eslint-disable-next-line no-console
-console.log('Hello Node.js starter!')
+import express from 'express'
+import cors from 'cors'
+
+const PORT: number = Number(process.env.PORT) || 5000
+
+const app = express()
+
+// Middlewares
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Home
+app.get('/', function (req, res) {
+  res.set('Content-Type', 'text/html')
+  res.send(
+    Buffer.from(
+      '<html><head><title>Node.js Starter</title></head><body><p>Node.js Starter</p></body></html>'
+    )
+  )
+})
+
+app.get('/hi', (req, res) => {
+  res.json({ message: 'Hi! Welcome to the Node.js starter template.' })
+})
+
+// Start server
+app.listen(PORT, () =>
+  // eslint-disable-next-line no-console
+  console.log(`🚀 Server ready at port ${PORT}`)
+)
